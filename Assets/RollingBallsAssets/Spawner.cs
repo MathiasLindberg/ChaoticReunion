034f2b38eecb2aac8a91270239421e7cb39dc95b
Konrad Mampe <konrad.mampe@gmail.com>
2022-04-30 22:32:01 +0200

    Better Spawner

--- Assets/RollingBallsAssets/Spawner.cs
+++ Assets/RollingBallsAssets/Spawner.cs
@@ -4,24 +4,64 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject gameObject;
+    [SerializeField] private List<GameObject> gameObjects = new();
     [SerializeField] private uint amount;
     // Start is called before the first frame update
 
+    [Header("Periodic Spawn Variables")] 
+    [SerializeField] private float spawnFrequency;
+    [SerializeField] private float spawnAmount;
+
+    private float _frequencyTimer;
+    
     public void Spawn()
     {
         var collider = GetComponent<Collider>();
         var min = collider.bounds.min;
         var max = collider.bounds.max;
 
-        if (gameObject)
+        var gameObj = gameObjects[Random.Range(0, gameObjects.Count - 1)];
+        if (gameObj)
         {
             for (int i = 0; i < amount; i++)
             {
                 Vector3 spawnPoint = collider.ClosestPoint(new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z)));
-                GameObject instance = Instantiate(gameObject);
+                GameObject instance = Instantiate(gameObj);
                 instance.transform.position = spawnPoint;
             }
         }
+
+        StartCoroutine(KeepSpawning());
+    }
+
+    private IEnumerator KeepSpawning()
+    {
+        var collider = GetComponent<Collider>();
+        var min = collider.bounds.min;
+        var max = collider.bounds.max;
+        
+        _frequencyTimer = spawnFrequency;
+        
+        while (GameManager.Instance.GameState.Equals(GameStates.Running))
+        {
+            _frequencyTimer -= Time.deltaTime;
+
+            if (_frequencyTimer <= 0)
+            {
+                for (var i = 0; i < spawnAmount; i++)
+                {
+                    var gameObj = gameObjects[Random.Range(0, gameObjects.Count - 1)];
+                    Vector3 spawnPoint = collider.ClosestPoint(new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z)));
+                    GameObject instance = Instantiate(gameObj);
+                    instance.transform.position = spawnPoint;
+                }
+
+                _frequencyTimer = spawnFrequency;
+            }
+            
+            yield return null;
+        }
+
+        yield return null;
     }
 }
