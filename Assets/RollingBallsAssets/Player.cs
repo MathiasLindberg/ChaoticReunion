88a177df9bec67befd2c4763e8cd331f485f3a8c
Daria Radu <dariaradur3@gmail.com>
2022-04-30 12:49:26 +0200

    Add LegoHub and acceleration moving

--- Assets/RollingBallsAssets/Player.cs
+++ Assets/RollingBallsAssets/Player.cs
@@ -10,35 +10,44 @@ public class Player : MonoBehaviour
     [SerializeField] private KeyCode rightKey = KeyCode.D;
     [SerializeField] private float rollForce = 10.0f;
     [SerializeField] private float movementForce = 10.0f;
+    [SerializeField] private AccelerationSensor accelerationSensor;
     private Rigidbody rb;
     private bool forwardKeyDown;
     private bool backKeyDown;
     private bool leftKeyDown;
     private bool rightKeyDown;
     private Camera mainCam;
+
+    // Acceleration Sensor variables
+    private Vector3 previousAcceleration;
+    private bool isSensorConnected;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         mainCam = Camera.main;
+        isSensorConnected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(forwardKey)) forwardKeyDown = true;
-        else if (Input.GetKeyUp(forwardKey)) forwardKeyDown = false;
-        if (Input.GetKeyDown(backKey)) backKeyDown = true;
-        else if (Input.GetKeyUp(backKey)) backKeyDown = false;
-        if (Input.GetKeyDown(leftKey)) leftKeyDown = true;
-        else if (Input.GetKeyUp(leftKey)) leftKeyDown = false;
-        if (Input.GetKeyDown(rightKey)) rightKeyDown = true;
-        else if (Input.GetKeyUp(rightKey)) rightKeyDown = false;
+        /*        if (Input.GetKeyDown(forwardKey)) forwardKeyDown = true;
+                else if (Input.GetKeyUp(forwardKey)) forwardKeyDown = false;
+                if (Input.GetKeyDown(backKey)) backKeyDown = true;
+                else if (Input.GetKeyUp(backKey)) backKeyDown = false;
+                if (Input.GetKeyDown(leftKey)) leftKeyDown = true;
+                else if (Input.GetKeyUp(leftKey)) leftKeyDown = false;
+                if (Input.GetKeyDown(rightKey)) rightKeyDown = true;
+                else if (Input.GetKeyUp(rightKey)) rightKeyDown = false;
 
-        if (forwardKeyDown || backKeyDown || leftKeyDown || rightKeyDown)
-        {
-            ApplyMotion();
-        }
+                if (forwardKeyDown || backKeyDown || leftKeyDown || rightKeyDown)
+                {
+                    ApplyMotion();
+                }*/
+
+
     }
 
     private void ApplyMotion()
@@ -56,5 +65,22 @@ public class Player : MonoBehaviour
 
         rb.AddForce(force * movementForce);
         rb.AddTorque(Vector3.Cross(force * rollForce, Vector3.down));
+    }
+
+    public void InitializeSensor()
+    {
+        isSensorConnected = true;
+        previousAcceleration = accelerationSensor.Acceleration;
+    }
+
+    public void ApplyAccelerationFromSensor()
+    {
+        Vector3 currentAcceleration = accelerationSensor.Acceleration;
+        currentAcceleration.y = 0;
+        Vector3 force = currentAcceleration * Time.fixedDeltaTime * 1000.0f;
+        rb.AddForce(force * movementForce);
+        rb.AddTorque(Vector3.Cross(force * rollForce, Vector3.down));
+
+        previousAcceleration = currentAcceleration;
     }
 }
